662fd6a0a9c49a3600c2f473daf8c47210c2d9c8
AJB1000 <venteslbc1000@gmail.com>
2025-11-14 12:59:56 +0100

    Mise à jour ven. 14 nov. 2025 12:59:56 CET

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* =========  CONFIGURATION  ========= */
-const VERSION = 'v5';                       // ← changez ici pour forcer la MAJ
+const VERSION = 'v6';                       // ← changez ici pour forcer la MAJ
 const CACHE_NAME = `meom-${VERSION}`;
 const BASE_URL = self.location.pathname.replace(/sw\.js$/, '');
 const SHELL_URLS = [
